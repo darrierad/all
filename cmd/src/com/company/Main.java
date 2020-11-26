f4501007f4bc455fe8b151022d814c66abf3b8d9
darrierad <75011219+darrierad@users.noreply.github.com>
2020-11-26 11:45:38 +0300

    Create Main.java

---
+++ cmd/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("given " + args.length + " args cmd");
+        System.out.println("list of args ");
+        for (int i = 0; i < args.length; i++)
+            System.out.println("args [" + i + "]: " + args[i]);
+    }
+}
